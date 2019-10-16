9c32dc87b46c3a8897a0d1479b9135d4c366b14c
Unknown <victor.ascuna@utec.edu.pe>
2019-10-16 15:11:09 -0500

    feat(OpenGL)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    //draw
+
+    glFlush();
+}
+
+void init(){
+    glClearColor(0.0,0.0,1.0,1.0);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB);
+
+    glutInitWindowPosition(200,100);
+    glutInitWindowSize(500,500);
+
+    glutCreateWindow("Window 1");
+
+    glutDisplayFunc(display);
+    init();
+
+
+    glutMainLoop();
     return 0;
 }
