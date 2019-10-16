d4f8da02be45576bea3bcd41539ef80727ddd041
Unknown <victor.ascuna@utec.edu.pe>
2019-10-16 15:38:32 -0500

    feat(Coordenadas)

---
+++ Coordenadas.h
@@ -0,0 +1,18 @@
+//
+// Created by VICTOR on 16/10/2019.
+//
+
+#ifndef UNTITLED26_COORDENADAS_H
+#define UNTITLED26_COORDENADAS_H
+
+class Coordenadas {
+public:
+    int X;
+    int Y;
+
+    Coordenadas(){}
+    Coordenadas(int _X,int _Y): X(_X), Y(_Y){}
+    ~Coordenadas(){}
+};
+
+#endif //UNTITLED26_COORDENADAS_H
