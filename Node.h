f9733dd20d46c809f40551ad2d1b450f9cadbaa9
Unknown <victor.ascuna@utec.edu.pe>
2019-10-16 15:10:45 -0500

    feat(Node)

---
+++ Node.h
@@ -0,0 +1,14 @@
+//
+// Created by VICTOR on 16/10/2019.
+//
+
+#ifndef UNTITLED26_NODE_H
+#define UNTITLED26_NODE_H
+
+class Node {
+public:
+    Node(){}
+    ~Node(){}
+};
+
+#endif //UNTITLED26_NODE_H
