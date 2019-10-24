c17e753bf9c9eb3e61f24ba7e305fae7e0669f1c
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-10-24 15:28:21 -0500

    feat(Functions):
    Density incomplete

--- Grapho.h
+++ Grapho.h
@@ -20,6 +20,12 @@ public:
     vector<Node<N,COOR>*> nodes;
     vector<Edge<N,COOR>*> edges;
     Grapho(){}
+
+    double density(){
+        nodes.size();
+        return result;
+    }
+
     ~Grapho(){}
 };
 
