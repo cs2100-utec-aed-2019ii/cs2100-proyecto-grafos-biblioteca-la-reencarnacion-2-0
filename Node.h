4fc8e73a25033cad1cfb46af7485c79370be15dc
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-10-24 15:19:35 -0500

    feat(Emergency Merge):
    Merge to develop

--- Node.h
+++ Node.h
@@ -5,9 +5,42 @@
 #ifndef UNTITLED26_NODE_H
 #define UNTITLED26_NODE_H
 
+#include "Coordenadas.h"
+#include <iostream>
+
+#define COOR 0
+#define INT 1
+#define LETRA 2
+
+template<typename N, int type>
 class Node {
 public:
-    Node(){}
+    Coordenadas coordenadas;
+    Node(float _X, float _Y): coordenadas(_X,_Y){}
+    ~Node(){}
+};
+
+template<typename N>
+class Node<N, COOR>{
+public:
+    N coordenadas;
+    Node(float _X, float _Y): coordenadas(_X,_Y){}
+    ~Node(){}
+};
+
+template<typename N>
+class Node<N, INT>{
+public:
+    N value;
+    Node(N _value): value(_value){}
+    ~Node(){}
+};
+
+template<typename N>
+class Node<N, LETRA>{
+public:
+    N value;
+    Node(N _value): value(_value){}
     ~Node(){}
 };
 
