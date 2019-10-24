e6f11a39b80e9dea42e492fdcc27f3989b188866
marlon <marlon.mejia@utec.edu.pe>
2019-10-24 15:30:45 -0500

    feat(traits): Node

--- Grapho.h
+++ Grapho.h
@@ -5,10 +5,56 @@
 #ifndef UNTITLED26_GRAPHO_H
 #define UNTITLED26_GRAPHO_H
 
-class Grafo {
+#include "Node.h"
+#include "Edge.h"
+#include <iostream>
+#include <vector>
+
+#define COOR 0
+#define INT 1
+#define LETRA 2
+
+template<typename N, typename T, int type>
+class Grapho {
 public:
-    Grafo(){}
-    ~Grafo(){}
+    vector<Node<N,COOR>*> nodes;
+    vector<Edge<N,COOR>*> edges;
+    Grapho(){}
+    ~Grapho(){}
+};
+
+template<typename N, typename T>
+class Grapho <N, T, COOR>{
+public:
+    vector<Node<N,COOR>*> nodes;
+    vector<Edge<T,COOR>*> edges;
+    Grapho(){}
+    void insert_Node(float _X, float _Y){
+        Node<N,COOR>* new_node = new Node<N,COOR>(_X,_Y);
+        nodes.push_back(new_node);
+    }
+    void insert_Edge(Node<N,COOR>* node_1, Node<N,COOR>* node_2, T weight){
+        Edge<T,COOR>* new_edge = new Edge<T,COOR>(node_1, node_2, weight);
+        edges.push_back(new_edge);
+    }
+
+    ~Grapho(){}
+};
+
+template<typename N, typename T>
+class Grapho <N, T, INT>{
+public:
+    Node<N,INT>* head;
+    Grapho(){}
+    ~Grapho(){}
+};
+
+template<typename N, typename T>
+class Grapho <N, T, LETRA>{
+public:
+    Node<N,LETRA>* head;
+    Grapho(){}
+    ~Grapho(){}
 };
 
 #endif //UNTITLED26_GRAPHO_H
---
+++ macros.h
@@ -0,0 +1,27 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <cstdio>
+#include <stdio.h>
+#include <string>   
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+// definiciones estandar para la ventana de opengl y limites de los Boids
+#define ANCHO	600
+#define ALTO	600
+#define PROFUNDIDAD 1200
+
+#define ORADIUS 10
+
+// duraccion del timer de glut.
+#define DURATION 10
+
+
+#endif 
