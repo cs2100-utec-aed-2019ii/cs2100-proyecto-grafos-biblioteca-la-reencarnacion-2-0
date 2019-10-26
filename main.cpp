2bf23cabec7ba4cf3ab6e571957decc8ea6cef78
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-10-26 13:42:58 -0500

    feat(General)

--- main.cpp
+++ main.cpp
@@ -19,20 +19,6 @@ using namespace std;
 #define LETRA 2
 
 Grapho<Coordenadas, int, COOR> grafo_0;
-<<<<<<< HEAD
-
-double rotacion = 0;
-double move_x = 0;
-double move_y = 0;
-
-// inicializamos glut
-GLvoid initGL(){
-    glClearColor(RED, GREEN, BLUE, ALPHA);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-}
-
-=======
 Grapho<Coordenadas, int, COOR> grafo_1("Prueba.txt");
 Grapho<Coordenadas, int, COOR> grafo_3(50,50);
 Grapho<Coordenadas, int, COOR> grafo_2(grafo_1);
@@ -50,7 +36,6 @@ GLvoid initGL(){
     glLoadIdentity();
 }
 
->>>>>>> Víctor
 GLvoid window_display(){
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
@@ -98,56 +83,6 @@ GLvoid window_reshape(GLsizei width, GLsizei height){
     glMatrixMode(GL_MODELVIEW);
 //	gluLookAt(0,50, 100, 0,0,0, 0, 1, 0);
 
-<<<<<<< HEAD
-}
-
-GLvoid window_key(unsigned char key, int x, int y){
-    switch (key) {
-        case ECHAP:
-            exit(1);
-            break;
-
-        default:
-            break;
-    }
-}
-
-// funcion de mouse, para colocar elementos en las coodenadas del mouse
-// problema::  coloca en otras coordenadas mas no en el x,y
-GLvoid callback_mouse(int button, int state, int x, int y){
-    if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON){
-        //boids.push_back(boid(x,600-y));
-        //predador =  vector_t(x, 600-y,0);
-    }
-}
-
-// timer , para demorar el tiempo de redibujado de glut
-// permite que veamos mas lentamente la simulacion
-void Timer	(int value){ // intervalo en miliseg
-    glutPostRedisplay	(	);
-    rotacion++;
-    move_x++;
-    move_y++;
-
-    //actualizar(); //ACTUALIZAMOS los BOIDS
-
-    glutTimerFunc		(DURATION,Timer, 10);
-}/// Timer
-
-
-
-int main (int argc, char* argv[]){
-    srand(time(NULL));
-
-    grafo_0.insert_Node(20,10);
-    grafo_0.insert_Node(500,30);
-    grafo_0.insert_Node(400,100);
-    grafo_0.insert_Edge(grafo_0.nodes[0],grafo_0.nodes[1],20);
-    grafo_0.insert_Edge(grafo_0.nodes[1],grafo_0.nodes[2],20);
-    grafo_0.insert_Edge(grafo_0.nodes[2],grafo_0.nodes[0],20);
-
-
-=======
 }
 
 GLvoid window_key(unsigned char key, int x, int y){
@@ -203,11 +138,7 @@ int main (int argc, char* argv[]){
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(ANCHO, ALTO);
     glutInitWindowPosition(100,100);
-<<<<<<< HEAD
-    glutCreateWindow("Boids!");
-=======
     glutCreateWindow("Grafo");
->>>>>>> Víctor
     initGL();
     glEnable(GL_TEXTURE_2D);
     glutDisplayFunc(&window_display);
@@ -216,6 +147,5 @@ int main (int argc, char* argv[]){
     glutTimerFunc(DURATION, Timer, 1);
     glutMouseFunc(&callback_mouse);
     glutMainLoop();
-
     return true;
 }
